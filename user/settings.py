04474e17ef908ad2ce1c98ec0956f26eb2e24b7d
sonusant <nsmahalead@gmail.com>
2021-12-15 21:14:35 +0530

    host added

--- user/settings.py
+++ user/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['https://nitin-user-dash.herokuapp.com/',
     'localhost',
-'127.0.0.1']
+'127.0.0.1',
+'nitin-user-dash.herokuapp.com']
 
 # Application definition
 
